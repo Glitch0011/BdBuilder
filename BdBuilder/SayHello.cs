f804a9b352ee930745a79982e10767d897de8f92
Tom Bonner <tom@bonner.is>
2018-01-01 14:29:57 +0000

    Large change to use tasks rather than calls

---
+++ BdBuilder/SayHello.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+
+namespace BdBuilder
+{
+    public class SayHello : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Test");
+            return true;
+        }
+    }
+}
